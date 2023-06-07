83f757bb130410470de63178acd409415a2fd882
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-06-08 00:22:35 +0100

    Project Update

--- src/Pages/Project/projectEndangeredSpecies.js
+++ src/Pages/Project/projectEndangeredSpecies.js
@@ -18,6 +18,15 @@ import Postman from "../../Images/skillsIcons/postman.svg";
 import GithubIcon from "../../Images/skillsIcons/githubIcon.png";
 import PlayIcon from "../../Images/skillsIcons/playIcon.png";
 
+import FontSizeIncreaserImg from "../../Images/projectsImage/fontSizeIncreaser.PNG";
+import TextToVoicImg from "../../Images/projectsImage/textToVoice.PNG";
+import ReadingGuideImg from "../../Images/projectsImage/readingGuide.PNG";
+import ReadingMaskImg from "../../Images/projectsImage/readingMask.PNG";
+import DrkLgtModeImg from "../../Images/projectsImage/darkmode.PNG";
+import LargeCurserImg from "../../Images/projectsImage/largeCurser.PNG";
+import QuizImg from "../../Images/projectsImage/quizFeature.PNG";
+
+
 function ProjectEndangeredSpecies (){
 
    
@@ -91,23 +100,81 @@ function ProjectEndangeredSpecies (){
             <h2> <span>02. </span>About this Project</h2>
 
             <section className="aboutSecton">
+                <p>
+                    This is my final year dissertion project.
+                    During my research, I have found out that there were lack of awareness about the endangered species around world.
+                    And there are millions of web applications facing lawsuits due to lack of usability features.
+                    This project is an interactive educational web application that raises awareness about the enangered species and how you can help them.
+                    For example, different species of endangered species, what are the threats, and, how you can support them.
+                    In addition, the web application provides features that provides usability features for visually impared users like colourblind and weak vision users.
+                </p>
+            </section>
 
-                <div className="windowFrame"> This is the window/Curtin.
+
+            <h2> <span>03. </span>Project Features</h2>
+
+            <section className="projectFeaturesSection">
+     
+                <div className="featureFrame">
+                    <image className="featureImage" style={{backgroundImage: `url(${FontSizeIncreaserImg})`}}></image>
+                    <div className="featureParagraphFrame">
+                        <h3>Font size increaser</h3>
+                        <p>Allows users with weak vision to increase the size of the texts inside the paragraphs.</p>
+                    </div>
+
+                    <div className="featureParagraphFrame">
+                        <h3>Text to voice</h3>
+                        <p>Allows users who finds reading difficult to convert the paragraphs texts into a voice.</p>
+                    </div>
+                    <image className="featureImage" style={{backgroundImage: `url(${TextToVoicImg})`}}></image>
+
+
+
+                    <image className="featureImage" style={{backgroundImage: `url(${ReadingMaskImg})`}}></image>
+                    <div className="featureParagraphFrame">
+                        <h3>Reading Mask</h3>
+                        <p>Allows the users to specifically focus on certian part of the paragraph, making it easy to focus and read.</p>
+                    </div>
+
+                    <div className="featureParagraphFrame">
+                        <h3>Reading Guide</h3>
+                        <p>Allows the users to keep track and follow along the lines of text, just like your thumb following a line of text.</p>
+                    </div>
+                    <image className="featureImage" style={{backgroundImage: `url(${ReadingGuideImg})`}}></image>
+
+                    <image className="featureImage" style={{backgroundImage: `url(${DrkLgtModeImg})`}}></image>
+                    <div className="featureParagraphFrame">
+                        <h3>Dark/light Mode</h3>
+                        <p>Allows the users to change the color theme of the whole web application to dark/light.</p>
+                    </div>
+
+                    <div className="featureParagraphFrame">
+                        <h3>Large Curser</h3>
+                        <p>Allows users with weak vision to change the normal mouse curser to large curser, making it easy to keep track of the curser.</p>
+                    </div>
+                    <image className="featureImage" style={{backgroundImage: `url(${LargeCurserImg})`}}></image>
+
+
+                    <image className="featureImage" style={{backgroundImage: `url(${QuizImg})`}}></image>
+                    <div className="featureParagraphFrame">
+                        <h3>Quiz</h3>
+                        <p>Allows the users to take quiz and check their knowledge on endangered species, based on the informations provided by the web application.</p>
+                    </div>
+                
+                </div>
+            </section>
+
+            <div className="windowFrame"> This is the window/Curtin.
                     <div className="windowDoorL"></div>
                     <div className="windowDoorR"></div>
                 </div>
 
-                <div className="cameraLenseFrame"> Camera Open Lense.
-                    <div className="coverTL"></div>
-                    <div className="coverTR"></div>
-                    <div className="coverBL"></div>
-                    <div className="coverBR"></div>
-                </div>
-
-            </section>
-
-            <h2> <span>03. </span>Project Features</h2>
-            
+            <div className="cameraLenseFrame"> Camera Open Lense.
+                <div className="coverTL"></div>
+                <div className="coverTR"></div>
+                <div className="coverBL"></div>
+                <div className="coverBR"></div>
+            </div>
         </div>
     );
 }
