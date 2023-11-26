1cc2da9bc052d92c3bc9be15b332f93a38cab1d8
Khagendra1814138 <75677824+Khagendra1814138@users.noreply.github.com>
2023-11-26 01:51:04 +0000

    samll

--- src/Pages/Project/projectKelang.js
+++ src/Pages/Project/projectKelang.js
@@ -7,7 +7,7 @@ import { useInView } from 'react-intersection-observer';
 
 import Back from "../../Images/dalmatian-spots.svg";
 
-import EcommerceCover from "../../Images/projectsImage/Ecommerce.PNG";
+import Kelang from "../../Images/projectsImage/Kelang.PNG";
 import ReactIcon from "../../Images/skillsIcons/reactIcon.png"
 import HtmlIcon from "../../Images/skillsIcons/htmlIcon.png";
 import CssIcon from "../../Images/skillsIcons/cssIcon.png";
@@ -18,7 +18,11 @@ import PhotopeaIcon from "../../Images/skillsIcons/photopeaIcon.png";
 import GithubIcon from "../../Images/skillsIcons/githubIcon.png";
 import PlayIcon from "../../Images/skillsIcons/playIcon.png";
 
-import FontSizeIncreaserImg from "../../Images/projectsImage/fontSizeIncreaser.PNG";
+import KelangFigmaDesign1 from "../../Images/projectsImage/KelangFigmaDesign1.PNG";
+import KelangFigmaDesign2 from "../../Images/projectsImage/KelangFigmaDesign2.PNG";
+import KelangFigmaDesign3 from "../../Images/projectsImage/KelangFigmaDesign3.PNG";
+
+import parallax from "../../Images/projectsImage/parallax.PNG";
 
 function ProjectKelang (){
 
@@ -46,7 +50,7 @@ function ProjectKelang (){
         <div className="KelangPage" style={{backgroundImage: `url(${Back})`}} > 
             <h1>Kelang an educaional web application.</h1>
 
-            <image style={{backgroundImage: `url(${EcommerceCover})`}} 
+            <image style={{backgroundImage: `url(${Kelang})`}} 
            
                 ref={headerImgRef} className={`${'projectImage'} ${headerImgIsVisible ? "showAnimateEndangeredSpecies" : ' '}`}
                 >
@@ -106,26 +110,40 @@ function ProjectKelang (){
             <section ref={aboutRef} className={`${'aboutSecton'} ${aboutIsVisible ? "showAnimateEndangeredSpecies" : ' '}`}>
                 <p>
                    This is an educational web application about a dance from an indigenous tribes of Eastern Nepal called the "Limbu" community.
+                   I wanted to create this project because this is about my own limbu culture dance. 
+                   With the advancement of tenchology and spread of western culture, the new generation of limbus are slowely starting to forgetting their own culture.
+                   So this web application is designed to answer many questions about the Kelang Dance and teach the new generations. This is a simple static content website.
                 </p>
             </section>
 
             <h2> <span>03. </span>Project Designs/Layout</h2>
-            <section  className='projectFeaturesSection'>
-                <Link exact to="https://www.figma.com/file/veXIbF7HltJTiDdWPJAswo/Kelang-UI-layout-design?type=design&node-id=0%3A1&mode=design&t=u3jxkpTlzRFrq3zJ-1" target="_blank" className="frame">
-                    <div className="card">
-                        <image style={{backgroundImage: `url(${PlayIcon})`}}></image>
-                    </div>
-                </Link>
+            <section  className='projectDesignSection'>
+                <div className="projectDesignFrame">
+                    <images className="projectDesignImages" style={{backgroundImage: `url(${KelangFigmaDesign1})`}}></images>
+                    <images className="projectDesignImages" style={{backgroundImage: `url(${KelangFigmaDesign2})`}}></images>
+                </div>
+                    
+                <div className="projectDesignFrame">
+                    <images className="projectDesignImages" style={{backgroundImage: `url(${KelangFigmaDesign3})`}}></images>
+                    <images className="projectDesignImages">
+                        <Link exact to="https://www.figma.com/file/veXIbF7HltJTiDdWPJAswo/Kelang-UI-layout-design?type=design&node-id=0%3A1&mode=design&t=u3jxkpTlzRFrq3zJ-1" target="_blank" className="frame">
+                            <div className="card">
+                                <image style={{backgroundImage: `url(${PlayIcon})`}}></image>
+                            </div>
+                        </Link>
+                    </images>
+                   
+                </div>
             </section>
 
             <h2> <span>04. </span>Project Features</h2>
             <section  className='projectFeaturesSection'>
                 <div className="featureFrame">
 
-                    <image className="featureImage" style={{backgroundImage: `url(${FontSizeIncreaserImg})`}}></image>
+                    <image className="featureImage" style={{backgroundImage: `url(${parallax})`}}></image>
                     <div className="featureParagraphFrame">
-                        <h3>Font size increaser</h3>
-                        <p>Allows users with weak vision to increase the size of the texts inside the paragraphs.</p>
+                        <h3>Cool Parallax</h3>
+                        <p>Cool parallax effect when the user move their mouse. The background moves to the opposite of the user mouse position and zooms in and out based on the mouse position left or right.</p>
                     </div>
 
                     <line></line>
